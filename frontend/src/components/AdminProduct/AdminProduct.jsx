db5b5dcd31c957acf682d3ec9055bf9de3c7fb27
NguyenMauThanhDat <128025273+NguyenMauThanhDat@users.noreply.github.com>
2024-11-25 22:55:12 +0700

    update filter

--- frontend/src/components/AdminProduct/AdminProduct.jsx
+++ frontend/src/components/AdminProduct/AdminProduct.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { DeleteOutlined, EditOutlined, PlusOutlined } from "@ant-design/icons";
+import React, { useEffect, useRef, useState } from "react";
+import { DeleteOutlined, EditOutlined, PlusOutlined,SearchOutlined } from "@ant-design/icons";
 import { WrapperHeader } from "./style";
-import { Button, Modal, Form, Input, Checkbox, Descriptions } from "antd";
+import { Button, Modal, Form, Input, Checkbox, Descriptions, Space } from "antd";
 import TableComponent from "../TableComponent/TableComponent";
 import InputComponent from "../InputComponent/InputComponent";
 import { WrapperUploadFile } from "../../pages/Profiles/style";
@@ -22,6 +22,9 @@ const AdminProduct = () => {
   const user=useSelector((state)=>state?.user)
   const [isOpenDrawer, setIsOpenDrawer] = useState(false)
   const [isModalOpenDelete, setIsModalOpenDelete] = useState(false)
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef(null);
   const [stateProduct, setStateProduct] = useState({
     name: "",
     price: "",
@@ -117,19 +120,139 @@ const renderAction = () =>{
     </div>
   )
 }
+const handleSearch = (selectedKeys, confirm, dataIndex) => {
+  confirm();
+  setSearchText(selectedKeys[0]);
+  setSearchedColumn(dataIndex);
+};
+const handleReset = (clearFilters) => {
+  clearFilters();
+  setSearchText('');
+};
+const getColumnSearchProps = (dataIndex) => ({
+  filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+    <div
+      style={{
+        padding: 8,
+      }}
+      onKeyDown={(e) => e.stopPropagation()}
+    >
+      <InputComponent
+        ref={searchInput}
+        placeholder={`Search ${dataIndex}`}
+        value={selectedKeys[0]}
+        onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+        onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+        style={{
+          marginBottom: 8,
+          display: 'block',
+        }}
+      />
+      <Space>
+        <Button
+          type="primary"
+          onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          icon={<SearchOutlined />}
+          size="small"
+          style={{
+            width: 90,
+          }}
+        >
+          Search
+        </Button>
+        <Button
+          onClick={() => clearFilters && handleReset(clearFilters)}
+          size="small"
+          style={{
+            width: 90,
+          }}
+        >
+          Reset
+        </Button>
+      </Space>
+    </div>
+  ),
+  filterIcon: (filtered) => (
+    <SearchOutlined
+      style={{
+        color: filtered ? '#1677ff' : undefined,
+      }}
+    />
+  ),
+  onFilter: (value, record) =>
+    record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+  filterDropdownProps: {
+    onOpenChange(open) {
+      if (open) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+  },
+  // render: (text) =>
+  //   searchedColumn === dataIndex ? (
+  //     <Highlighter
+  //       highlightStyle={{
+  //         backgroundColor: '#ffc069',
+  //         padding: 0,
+  //       }}
+  //       searchWords={[searchText]}
+  //       autoEscape
+  //       textToHighlight={text ? text.toString() : ''}
+  //     />
+  //   ) : (
+  //     text
+  //   ),
+});
+
+
 const columns = [
   {
     title: 'Name',
     dataIndex: 'name',
-    render: (text) => <a>{text}</a>,
+    sorter : (a,b)=> a.name.length - b.name.length,
+    ...getColumnSearchProps('name')
   },
   {
     title: 'Price',
     dataIndex: 'price',
+    sorter : (a,b)=> a.price - b.price,
+    filters: [
+      {
+        text: '>=100',
+        value: '>=',
+      },
+      {
+        text: '<=100',
+        value: '<=',
+      }
+    ],
+    onFilter: (value, record) => {
+      if(value==='>='){
+        return record.price >= 100
+      } 
+         return record.price<= 100
+    },
   },
   {
     title: 'Rating',
     dataIndex: 'rating',
+    sorter : (a,b)=> a.rating- b.rating,
+    filters: [
+      {
+        text: '>=3',
+        value: '>=',
+      },
+      {
+        text: '<=3',
+        value: '<=',
+      }
+    ],
+    onFilter: (value, record) => {
+      if(value==='>='){
+        return record.rating >= 3
+      } 
+         return record.rating<= 3
+    },
   },
   {
     title: 'Type',
